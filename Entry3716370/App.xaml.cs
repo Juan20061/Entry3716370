b7b98af0e33eefeecdd79dd35ec9dd4594b5a7f6
Juan Carlos <Juan Carlos@DESKTOP-QQANMR9>
2024-05-28 14:21:29 -0600

    Agregar archivos de proyecto.

---
+++ Entry3716370/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Entry3716370
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
